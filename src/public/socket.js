074a7bad61931dbb7b67475fed5146f57d68bb94
sonvk123 <70042474+sonvk123@users.noreply.github.com>
2024-02-19 16:17:06 +0700

    Update socket.js

--- src/public/socket.js
+++ src/public/socket.js
@@ -5,10 +5,10 @@ module.exports = {
     io = require("socket.io")(httpServer, {
       cors: {
         origin: [
-          "http://localhost:3000",
-          "http://localhost:3001",
-          "https://fe-admin-ass3-nodejs.onrender.com",
-          "https://fe-client-ass3-nodejs.onrender.com",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://main--adminass3nodejs.netlify.app",
+    "https://fe-client-ass3-nodejs.onrender.com",
         ],
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       },
